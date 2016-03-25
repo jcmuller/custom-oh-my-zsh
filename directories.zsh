65e8effd49d9baa538725918c39caec01f349f17
Juan C. Müller <jcmuller@gmail.com>
2016-03-25 11:18:23 -0400

    Add directories

--- directories.zsh
+++ directories.zsh
@@ -1,12 +1,18 @@
 DEV_SRC=~/Development/Greenhouse
 
-ansible=$DEV_SRC/ansible
+ansible=/infra/ansible/ansible
 api=$DEV_SRC/dajoku_api
 cli=$DEV_SRC/dajoku_cli
 configs=$DEV_SRC/dajoku_configs
+dajoku_api=$DEV_SRC/dajoku_api
+dajoku_cli=$DEV_SRC/dajoku_cli
+dajoku_configs=$DEV_SRC/dajoku_configs
 dajoku_consul=$DEV_SRC/dajoku_consul
 greenhouse=$DEV_SRC/greenhouse
 grnhse=$DEV_SRC/greenhouse
 infra=$DEV_SRC/infrastructure
+infra_scripts=$DEV_SRC/infra-scripts
+infrastructure=$DEV_SRC/infrastructure
 run_container=$DEV_SRC/run_container
 scripts=$DEV_SRC/infra-scripts
+jben=$DEV_SRC/jben
