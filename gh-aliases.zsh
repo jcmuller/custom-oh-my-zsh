d8dd3d8b33e0ae5b33048b3c4f0c38097fb600c5
Juan C. Müller <jcmuller@gmail.com>
2018-02-06 10:51:05 -0500

    Remove stuff

--- gh-aliases.zsh
+++ gh-aliases.zsh
@@ -1,11 +1,2 @@
-alias devlog="heroku logs --tail -n10000 --app dev-greenhouse"
-alias uatlog="heroku logs --tail -n10000 --app uat-greenhouse"
-
-alias devc="heroku run rails console --app dev-greenhouse"
-alias uatc="heroku run rails console --app uat-greenhouse"
-
-alias rspec="bundle exec rspec"
-alias rake="bundle exec rake"
 alias testdb="RAILS_ENV=test bundle exec rake -t db:test:load db:seed"
-
 alias ghdropbox="HOME=/home/jcmuller/.gh-home dropbox"
