a5844c5405b0850d0fff6830a92c27ad7caf6e8f
Juan C. Müller <jcmuller@gmail.com>
2019-03-29 21:33:29 -0400

    Fix goroot/bin

--- goenv.zsh
+++ goenv.zsh
@@ -4,12 +4,18 @@ GOPATH=~/go; export GOPATH
 eval "$($GOENV_ROOT/bin/goenv init -)"
 [ -s "$GOENV_ROOT/completions/goenv.zsh" ] && . "$GOENV_ROOT/completions/goenv.zsh"
 
-GOROOT="$(goenv prefix)"; export GOROOT
-
 PATH_COMPONENTS=(
 "$GOENV_ROOT/bin"
 "$GOENV_ROOT/shims"
 "$GOPATH/bin"
+)
+
+PATH="${(j/:/)PATH_COMPONENTS}:${PATH}"
+export PATH
+
+GOROOT="$(goenv prefix)"; export GOROOT
+
+PATH_COMPONENTS=(
 "$GOROOT/bin"
 )
 
