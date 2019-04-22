a1e96b50215fa79ddc54894629c2ae8dc0e4dfdc
Juan C. Müller <jcmuller@gmail.com>
2019-04-22 11:19:21 -0400

    configure aws-vault with pass

--- aws.zsh
+++ aws.zsh
@@ -1,3 +1,4 @@
-AWS_VAULT_BACKEND=secret-service; export AWS_VAULT_BACKEND
+AWS_VAULT_BACKEND=pass; export AWS_VAULT_BACKEND
+AWS_VAULT_PASS_PREFIX=aws-vault; export AWS_VAULT_PASS_PREFIX
+AWS_VAULT_PASS_CMD=pass; export AWS_VAULT_CMD
 alias aws-vault-pass='aws-vault --backend=pass --pass-cmd=pass --pass-prefix=aws-vault'
-alias aws-vault-secret-service='aws-vault --backend=secret-service'
