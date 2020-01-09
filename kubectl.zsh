73f618cf23ba65fe55e94642a43dea6f1be64218
Juan C. Müller <jcmuller@gmail.com>
2020-01-09 10:16:10 -0500

    Add alias

--- kubectl.zsh
+++ kubectl.zsh
@@ -1,5 +1,6 @@
 alias k=kubectl
 alias m=minikube
+alias tf=terraform
 
 kdebug () {
     kubectl run -it --rm --labels "consumer=juan,kdebug=true" --image gcr.io/gh-infra/debug-tools "debug-juan" "$@"
