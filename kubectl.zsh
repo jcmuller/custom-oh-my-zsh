60628f6f8e3f8230f2bf07b7c7ed2c18fc7e967f
Juan C. Müller <jcmuller@gmail.com>
2020-02-21 13:59:40 -0500

    Add some argo aliases

--- kubectl.zsh
+++ kubectl.zsh
@@ -1,6 +1,12 @@
 alias k=kubectl
 alias m=minikube
 alias tf=terraform
+alias a="argo -n argo"
+alias alr="a list --running"
+alias alrnh="alr --no-headers"
+alias alrnhno="alrnh -o name"
+alias at='a terminate $(alrnhno | fzf)'
+alias al='a logs -fw `alrnhno | fzf`'
 
 kdebug () {
     kubectl run -it --rm --labels "consumer=juan,kdebug=true" --image gcr.io/gh-infra/debug-tools "debug-juan" "$@"
