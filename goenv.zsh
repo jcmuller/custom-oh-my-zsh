34881d13770ffcb2eaa4af32e85f596f90a10aae
Juan C. Müller <jcmuller@gmail.com>
2020-03-05 17:11:01 -0500

    Set goroot and gopath

--- goenv.zsh
+++ goenv.zsh
@@ -1,17 +1,11 @@
-GOENV_ROOT="$HOME/.goenv"; export GOENV_ROOT
 GOPATH=~/go; export GOPATH
 
-eval "$($GOENV_ROOT/bin/goenv init -)"
-[ -s "$GOENV_ROOT/completions/goenv.zsh" ] && . "$GOENV_ROOT/completions/goenv.zsh"
-
 path=(
-  "$GOENV_ROOT/bin"
-  "$GOENV_ROOT/shims"
   "$GOPATH/bin"
   "${path[@]}"
 )
 
-GOROOT="$(goenv prefix)"; export GOROOT
+GOROOT="~/go"; export GOROOT
 
 for bin in $(find "$HOME/go" -maxdepth 2 -type d -name bin); do
 path=(
@@ -19,8 +13,3 @@ path=(
   "${path[@]}"
 )
 done
-
-path=(
-  "$GOPATH/$(goenv version-name)/bin"
-  "${path[@]}"
-)
