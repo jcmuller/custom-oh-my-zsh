21b896a36b0cd8a217ed9beb305511dfc8c36c75
Juan C. Müller <jcmuller@gmail.com>
2020-11-20 12:35:38 -0500

    Remove old aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,20 +1,3 @@
-alias GET="lwp-request -m get"
-alias HEAD="lwp-request -m head"
-alias POST="lwp-request -m post"
-
-alias b="nocorrect bundle"
-alias be="b exec"
-alias bec="be cucumber"
-alias ber="be rspec"
-alias bes="be spork"
-alias besc="bes cucumber"
-alias besr="bes rspec"
-alias bi="b install"
-alias biall='for i in /web/*; do (echo "$i:"; cd $i && (b check || b)); done'
-alias bu="b update"
-alias bum="bu models"
-alias bumall='for i in /web/*; do (echo "$i:"; cd $i && bum); done'
-
 alias gitl='git log --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%an, %cr)%Creset'\'' --abbrev-commit --date=relative'
 alias gitls='gitl --max-count=5'
 alias gpr="git pull --rebase"
@@ -27,18 +10,6 @@ alias gcv='git commit -v'
 alias gca='gcv -a'
 alias gco='git checkout'
 alias gcm='gco master'
-
-alias spork="nocorrect spork"
-alias rspec="nocorrect rspec"
-
-alias rc="rails c"
-
-alias zeus="nocorrect zeus"
-alias z="zeus"
-alias zs="z server"
-alias zc="z console"
-alias zr="z rake"
-alias zs="z rspec"
 
 alias codeclimate="docker run --interactive --tty --rm --env CODE_PATH="$PWD" --volume "$PWD":/code --volume /var/run/docker.sock:/var/run/docker.sock --volume /tmp/cc:/tmp/cc codeclimate/codeclimate"
 alias autometrics='docker run -it --rm --network=host gcr.io/gh-infra/autometrics-cli'
