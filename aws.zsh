468503c1a658acbae626942c7e8d2f1075762ba6
Juan C. Müller <jcmuller@gmail.com>
2021-03-31 09:29:35 -0400

    No need to alias aws-vault-use anymore

--- aws.zsh
+++ aws.zsh
@@ -2,27 +2,3 @@ AWS_VAULT_BACKEND=pass; export AWS_VAULT_BACKEND
 AWS_VAULT_PASS_PREFIX=aws-vault; export AWS_VAULT_PASS_PREFIX
 AWS_VAULT_PASS_CMD=pass; export AWS_VAULT_CMD
 AWS_SDK_LOAD_CONFIG=true; export AWS_SDK_LOAD_CONFIG
-
-aws-vault-use() {
-  local role ttl
-
-  role="${1:-support}"
-  duration="${2:-1h}"
-
-  unset AWS_VAULT
-
-  eval $(aws-vault exec \
-    "--mfa-token=$(gopass otp -o amazon.com/aws/juan@greenhouse.io)" \
-    "--duration=$duration" \
-    "$role" -- env |
-    awk '/^AWS/ { print "export " $1 }')
-}
-
-alias av=aws-vault
-alias aws-vault-pass='av --backend=pass --pass-cmd=pass --pass-prefix=aws-vault'
-alias aves='av exec --mfa-token=$(pass otp amazon.com/aws/juan@greenhouse.io) support -- '
-alias avesbe='aves bundle exec'
-alias avesber='avesbe rspec'
-alias avesxber='aves xvfb-run bundle exec rspec'
-alias avu=aws-vault-use
-alias avus="avu support"
