dceac3de7d1bd9b6b44254bdcb361e6761e5471d
Juan C. Müller <jcmuller@gmail.com>
2021-03-31 09:23:45 -0400

    Tell AWS SDK to load config

--- aws.zsh
+++ aws.zsh
@@ -1,6 +1,7 @@
 AWS_VAULT_BACKEND=pass; export AWS_VAULT_BACKEND
 AWS_VAULT_PASS_PREFIX=aws-vault; export AWS_VAULT_PASS_PREFIX
 AWS_VAULT_PASS_CMD=pass; export AWS_VAULT_CMD
+AWS_SDK_LOAD_CONFIG=true; export AWS_SDK_LOAD_CONFIG
 
 aws-vault-use() {
   local role ttl
